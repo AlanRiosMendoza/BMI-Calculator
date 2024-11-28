ba05263ca0a8271f94627dd8dd31eb581b321e42
Alan Rios <alan.rios.hd@gmail.com>
2024-11-28 14:11:38 -0500

    Eliminación de comentarios innecesarios en ClienteRMI.java y ServidorRMI.java

--- ClienteRMI.java
+++ ClienteRMI.java
@@ -4,21 +4,16 @@ import java.util.Scanner;
 public class ClienteRMI {
     public static void main(String[] args) {
         try {
-            // Get the RMI registry
             Registry registry = java.rmi.registry.LocateRegistry.getRegistry("localhost", 1099);
-            // Look up the remote object
             BMIRemote bmiRemote = (BMIRemote) registry.lookup("BMIService");
-            // Get the user input
             Scanner input = new Scanner(System.in);
             System.out.print("Ingresa tu peso en kg: ");
             double weight = input.nextDouble();
             System.out.print("Ingresa tu altura en m: ");
             double height = input.nextDouble();
-            // Invoke the remote method
             double bmi = bmiRemote.getBMI(weight, height);
             bmi = Math.round(bmi * 100.0) / 100.0;
             String category = bmiRemote.getBMICategory(bmi);
-            // Display the result
             System.out.println("Tu BMI es " + bmi);
             System.out.println("Tu categoría de BMI es " + category);
             
--- ServidorRMI.java
+++ ServidorRMI.java
@@ -3,9 +3,7 @@ import java.rmi.registry.Registry;
 public class ServidorRMI {
     public static void main(String[] args) {
         try {
-            // Create a BMI remote object
             BMIRemoteImple bmiRemote = new BMIRemoteImple();
-            // Register the remote object with the RMI Registry
             Registry registry = java.rmi.registry.LocateRegistry.createRegistry(1099);
             registry.rebind("BMIService", bmiRemote);
             System.out.println("Server is ready.");
